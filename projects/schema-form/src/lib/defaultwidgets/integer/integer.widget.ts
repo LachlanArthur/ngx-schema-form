479783463689d4f4355fd11eb4ed77d726702979
Lachlan Arthur <lachlan@nichestudio.com.au>
2018-07-31 20:22:24 +1000

    Copy `ngAfterViewInit` method to IntegerWidget

--- projects/schema-form/src/lib/defaultwidgets/integer/integer.widget.ts
+++ projects/schema-form/src/lib/defaultwidgets/integer/integer.widget.ts
@@ -19,4 +19,25 @@ import { ControlWidget } from '../../widget';
   [attr.minLength]="schema.minLength || null">
 </div>`
 })
-export class IntegerWidget extends ControlWidget {}
+export class IntegerWidget extends ControlWidget {
+  ngAfterViewInit() {
+    const control = this.control;
+    this.formProperty.valueChanges.subscribe((newValue) => {
+      if (control.value !== newValue) {
+        control.setValue(newValue, {emitEvent: false});
+      }
+    });
+    this.formProperty.errorsChanges.subscribe((errors) => {
+      control.setErrors(errors, { emitEvent: true });
+      const messages = (errors || [])
+        .filter(e => {
+          return e.path && e.path.slice(1) === this.formProperty.path;
+        })
+        .map(e => e.message);
+      this.errorMessages = messages.filter((m, i) => messages.indexOf(m) === i);
+    });
+    control.valueChanges.subscribe((newValue) => {
+      this.formProperty.setValue(newValue, false);
+    });
+  }
+}
